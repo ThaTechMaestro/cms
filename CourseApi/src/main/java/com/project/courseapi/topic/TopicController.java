41ab5afca60cab08d52bc72c123c3d77eaa82c68
ThaTechMaestro <muse.pearls@gmail.com>
2021-10-30 17:34:57 +0100

    version1: working CRUD implementation

--- CourseApi/src/main/java/com/project/courseapi/topic/TopicController.java
+++ CourseApi/src/main/java/com/project/courseapi/topic/TopicController.java
@@ -1,15 +1,49 @@
 package com.project.courseapi.topic;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Arrays;
+import java.util.List;
 
 @RestController
 public class TopicController {
 
-    @RequestMapping("/topics")
-    public String getAllTopics(){
+    @Autowired
+    private TopicService topicService;
 
-        return "All Topics";
+    // Access all topics (Read)
+    @RequestMapping("/topics")
+    public List<Topic> getAllTopics(){
+
+        return topicService.getAllTopics();
 
     }
+
+    // Access a particular topic (Read)
+    @RequestMapping("/topics/{id}")
+    public Topic getTopic(@PathVariable String id){
+        return topicService.getTopic(id);
+    }
+
+    // Add a new topic (Create)
+    @RequestMapping(method=RequestMethod.POST, value="/topics")
+    public void addTopic(@RequestBody Topic topic){
+        topicService.addTopic(topic);
+    }
+
+    // Make edits to a topic label (Update)
+    @RequestMapping(method=RequestMethod.PUT, value="/topics/{id}")
+    public void addTopic(@RequestBody Topic topic, @PathVariable String id){
+        topicService.updateTopic(id, topic);
+    }
+
+    // Delete a particular Topic (Delete)
+    @RequestMapping(method=RequestMethod.DELETE, value="/topics/{id}")
+    public void addTopic(@PathVariable String id){
+        topicService.deleteTopic(id);
+    }
+
+
+
 }
--- README.md
+++ README.md
@@ -1,2 +1,28 @@
 # cms
 A RESTful api for a course management System
+
+<hr/>
+
+
+# Development: Version 1
+## Overview
+* First working version for the Course Management API
+* It can be seen with the commit "version1:commit_message"
+
+* Development of the Topic Resource
+
+## Implementing the following CRUD features for the Topic resource
+- Accessing all the available topics
+-  Accesing a single topic
+- Add a paritcular topic
+- Updating a particular topic
+- Deleting a particular topic
+
+## Data Layer:
+* No database was used, the topic was created as an object utilising a service class implementation.
+* Each topic added as an object is stored in a list, which is created on request but only once via a service class(Singleton).
+
+## Concepts Learned
+* A fundamental understanding of RESTful APIs work and implementing different CRUD operations
+
+<hr/>
